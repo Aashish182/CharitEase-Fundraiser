7d45a2f720872eb526bd9a07660d48068e09587e
Aashish Gupta <aashishgupta99300@gmail.com>
2025-02-10 18:47:54 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ import AllBankDetails from './pages/AllBankDetails';
 
 function App() {
   const dispatch = useDispatch();
-
+  const [userDetails, setUserDetails] = useState(null);
   const fetchUserDetails = async() => {
     const dataResponse = await fetch(SummaryApi.current_user.url,{
       method: SummaryApi.current_user.method,
